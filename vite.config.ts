1e19abac25b14529362b158cd587ac36582579f8
NazarMatkovskyi <matkovskuyn@gmail.com>
2026-03-03 22:52:52 +0200

    Added build 2.0

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(() => ({
-  base: "/",
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/ARS-Racing/" : "/",
 
   server: {
     host: "::",
