b5da5a9afc928fb5d8da64a6ae3f753872f70610
NazarMatkovskyi <matkovskuyn@gmail.com>
2026-03-03 22:59:20 +0200

    Added build 2.0

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig(({ command }) => ({
 
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "/src"),
     },
   },
 }));
