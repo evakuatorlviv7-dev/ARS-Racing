3d98ceb57baa5c5ba6b8d57f60bd92fb55ebe871
evakuatorlviv7-dev <evakuatorlviv7@gmail.com>
2026-03-05 20:39:07 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/ARS-Racing/" : "/",
+  base: "/",
 
   server: {
     host: "::",
